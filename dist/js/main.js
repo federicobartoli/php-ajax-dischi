212b9e7d15ed7c2151b54da21f1639b22ead660b
Federico <federicobartoli@me.com>
2020-04-20 16:32:02 +0200

    Inzio template per handlebars

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+     var mySwiper = new Swiper ('.swiper-container', {
+       // Optional parameters
+       direction: 'horizontal',
+       loop: true,
+       autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+      },
+       keyboard: {
+        enabled: true,
+      },
+       // If we need pagination
+       pagination: {
+            el: '.swiper-pagination',
+           clickable: true
+       },
+
+       // Navigation arrows
+       navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+       },
+
+
+     })
+});
---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+     <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+          <link rel="stylesheet" href="dist/css/swiper.min.css">
+          <link rel="stylesheet" href="dist/css/style.css">
+          <script src="dist/js/jquery.min.js" charset="utf-8"></script>
+          <title>Php-ajax-dischi</title>
+     </head>
+     <body>
+          <header>
+
+               <div class="container-fluid container-fluid-up  clearfix">
+                    <div class="flex-container flex-center title">
+                         <h1>
+                              Bartolify
+                         </h1>
+                    </div>
+               </div>
+
+
+          </header>
+
+
+
+
+          <main>
+               <div class="container-fluid main bg-black">
+                    <div class="container title-main">
+                         <div class="flex-container flex-center title-main">
+                              <div class="swiper-container">
+                                  <!-- Additional required wrapper -->
+                                  <div class="swiper-wrapper">
+                                       <div class="swiper-slide">
+                                           <div class="card" style="background-image: url('https://m.media-amazon.com/images/I/91rEY0JbQML._SS500_.jpg') ;">
+                                                <div class="div-album">
+                                                     <img src="https://m.media-amazon.com/images/I/91rEY0JbQML._SS500_.jpg" alt="">
+
+                                                </div>
+                                                <div class="div-descrizioni">
+                                                     <h2>Highway 45</h2>
+                                                     <p>Federico Bartoli</p>
+                                                     <p>2018</p>
+                                                </div>
+                                           </div>
+                                      </div>
+                                      <div class="swiper-slide">
+                                          <div class="card" style="background-image: url('https://m.media-amazon.com/images/I/91rEY0JbQML._SS500_.jpg') ;">
+                                               <div class="div-album">
+                                                    <img src="https://m.media-amazon.com/images/I/91rEY0JbQML._SS500_.jpg" alt="">
+
+                                               </div>
+                                               <div class="div-descrizioni">
+                                                    <h2>Highway 45</h2>
+                                                    <p>Federico Bartoli</p>
+                                                    <p>2018</p>
+                                               </div>
+                                          </div>
+                                     </div>
+                                     <div class="swiper-slide">
+                                         <div class="card" style="background-image: url('https://m.media-amazon.com/images/I/91rEY0JbQML._SS500_.jpg') ;">
+                                              <div class="div-album">
+                                                   <img src="https://m.media-amazon.com/images/I/91rEY0JbQML._SS500_.jpg" alt="">
+
+                                              </div>
+                                              <div class="div-descrizioni">
+                                                   <h2>Highway 45</h2>
+                                                   <p>Federico Bartoli</p>
+                                                   <p>2018</p>
+                                              </div>
+                                         </div>
+                                    </div>
+                                  </div>
+                                  <!-- If we need pagination -->
+                                  <div class="swiper-pagination"></div>
+
+
+                                  <!-- If we need scrollbar -->
+                                  <div class="swiper-scrollbar"></div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </main>
+
+
+          <!-- <div class="swiper-slide">
+              <div class="card" style="background-image: url('https://m.media-amazon.com/images/I/91rEY0JbQML._SS500_.jpg') ;">
+                   <div class="div-album">
+                        <img src="https://m.media-amazon.com/images/I/91rEY0JbQML._SS500_.jpg" alt="">
+
+                   </div>
+                   <div class="div-descrizioni">
+                        <h2>Highway 45</h2>
+                        <p>Federico Bartoli</p>
+                        <p>2018</p>
+                   </div>
+              </div>
+         </div> -->
+     <script src="dist/js/main.js" charset="utf-8"></script>
+     <script src="dist/js/swiper.min.js" charset="utf-8"></script>
+     </body>
+</html>
